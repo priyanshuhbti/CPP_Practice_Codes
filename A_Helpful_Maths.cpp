3108bfd851e3f26ff2a5d8572f2b312e904a44c6
unknown <priyanshusgvm@gmail.com>
2024-08-01 21:27:44 +0530

    amin

---
+++ A_Helpful_Maths.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main()
+{
+    cin.tie(0);cin.sync_with_stdio(0);
+    cout.tie(0);cout.sync_with_stdio(0);
+    string s;
+    cin>>s;
+    sort(s.begin(),s.end());
+    cout<<s;
+
+    
+    return 0;
+}
