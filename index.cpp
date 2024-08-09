147cd83a86b61379df200c67b34e9269f6b8c6dd
priyanshuhbti <220105038@hbtu.ac.in>
2024-08-09 19:07:59 +0530

    Linux_work

---
+++ index.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+cout<<"Hello World"<<endl;
+
+return 0;
+// if you want to shift to the linux and only do code force in the linxu to learn ot ..
+// then ok good .. go for it .. and do dsa ..
+//Windows 11  Norebook is also setuped for doing it ..
+//Linux is setupped for learningn dSA jsut learn dsa..
+
+
+}
