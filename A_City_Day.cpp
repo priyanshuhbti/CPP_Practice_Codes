fa6fad662a6476b7a7268691827ccabe01307c93
Priyanshu Yadav <priyanshusgvm@gmail.com>
2024-08-15 16:14:44 +0530

    main

---
+++ A_City_Day.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main()
+{
+    cin.tie(0);cin.sync_with_stdio(0);
+    cout.tie(0);cout.sync_with_stdio(0);
+    
+    return 0;
+}
