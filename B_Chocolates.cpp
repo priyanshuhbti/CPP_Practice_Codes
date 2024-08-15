7948a53542c8a4e07ccd73c53ee5553166d05e49
Priyanshu Yadav <priyanshusgvm@gmail.com>
2024-08-15 12:05:09 +0530

    main

---
+++ B_Chocolates.cpp
@@ -0,0 +1,11 @@
+//For the normal task ==>Win11 and For Programming and Deep coding only LInux ..ubuntu LTS 20
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main()
+{
+    cin.tie(0);cin.sync_with_stdio(0);
+    cout.tie(0);cout.sync_with_stdio(0);
+   
+    return 0;
+}
