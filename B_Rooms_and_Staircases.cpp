23fc91b823274c7ceddc550950410dcd6789d536
Priyanshu Yadav <priyanshusgvm@gmail.com>
2024-08-16 23:48:24 +0530

    main

--- B_Rooms_and_Staircases.cpp
+++ B_Rooms_and_Staircases.cpp
@@ -1,15 +1,20 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-int main()
-{
-    cin.tie(0);cin.sync_with_stdio(0);
-    cout.tie(0);cout.sync_with_stdio(0);
-    int t = 1;
-    cin >> t;
-    while (t--)
-    {
-        
-    }
-    return 0;
+void solve(){
+	int n;
+	cin>>n;
+	string s;
+	cin>>s;
+	int mx=n;
+	for(int i=0;i<n;i++){
+		if(s[i]=='1')
+			mx=max(mx,2*max(i+1,n-i));
+	}
+	cout<<mx<<endl;
+}
+int main(){
+	int n;
+	cin>>n;
+	while(n--) solve();
+	return 0;
 }
