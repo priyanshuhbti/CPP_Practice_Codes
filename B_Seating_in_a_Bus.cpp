4ec554c64502c897f480175a961119c4a3875be1
unknown <priyanshusgvm@gmail.com>
2024-08-20 13:04:42 +0530

    main

--- B_Seating_in_a_Bus.cpp
+++ B_Seating_in_a_Bus.cpp
@@ -1,16 +1,19 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-int main()
-{
-    cin.tie(0);cin.sync_with_stdio(0);
-    cout.tie(0);cout.sync_with_stdio(0);
-    int t = 1;
-    cin >> t;
-    while (t--)
-    {
 
-windows was also good as .. akhir mere ek hi windows notebook se sara kaam ho jaa rha tha.. aab ssb aalg alg krna pd trha hai ..
-but prof is using in the dual boot so i should also use it  }
-    return 0;
+int main() {
+    int t = 1; cin>>t;
+    while(t--) {
+        int n; cin>>n;
+        string s; cin>>s;
+        bool check  =true;
+        for(int i=0; i<n/2; i++) {
+            if(s[i]==s[n-1-i]) continue;
+            else if(abs(s[i]-s[n-1-i])==2) continue;
+            check = false;
+            break;
+        }
+        cout<<(check?"YES":"NO")<<endl;
+        //The Server File Commited...
+    }
 }
---
+++ sample.cpp
@@ -0,0 +1,12 @@
+#include <iostream >
+using namespace std;
+
+int main(){
+ 
+
+cout<<"Name"<<endl;
+
+ return 0;
+
+
+}
