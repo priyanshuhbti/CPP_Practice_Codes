28d4babede18172d6a1df97d0e941450034f59ce
unknown <priyanshusgvm@gmail.com>
2024-08-22 23:16:30 +0530

    amin

---
+++ A_Ichihime_and_Triangle.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main()
+{
+    //Priyanshu Yadav || COmputers GEnius ||Adobe  Systems
+    cin.tie(0);cin.sync_with_stdio(0);
+    cout.tie(0);cout.sync_with_stdio(0);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+         int a,b,c,d ;
+        cin>>a>>b>>c>>d;
+     
+     cout<<b<<" "<<c<< " "<<c <<endl;
+     
+
+    }
+    return 0;
+}
