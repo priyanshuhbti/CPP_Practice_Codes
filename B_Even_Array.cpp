c497f6b8ee17057ad740d5e9a242341261fff64d
unknown <priyanshusgvm@gmail.com>
2024-08-27 16:48:19 +0530

    amin

---
+++ B_Even_Array.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main()
+{
+    cin.tie(0);cin.sync_with_stdio(0);
+    cout.tie(0);cout.sync_with_stdio(0);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+         
+        
+        cin>>n;int x;
+       int odd=0, even=0; 
+       for(int i=0; i<n; i++){
+       
+               cin>>x;
+               if(x%2 != i%2){
+                        if(x&1) odd++;
+                        else even++;
+
+               }
+              
+       }
+        if(odd==even){
+                cout<<odd<<endl;
+
+               }else{
+                 cout<<-1<<endl;
+
+               }
+
+    }
+    return 0;
+}
