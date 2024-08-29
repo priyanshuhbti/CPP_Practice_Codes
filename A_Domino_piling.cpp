29c614d863ec662582c75ba4f2c854a49950de05
priyanshuhbti <priyanshusgvm@gmail.com>
2024-08-29 22:02:27 +0530

    main

---
+++ A_Domino_piling.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+//Bss last ke 10 months hai..
+//Pesonlity like IITians 
+//And khub daba kr padho ..
+//UPSC IPS 
+//Programming 
+//ECE 
+    int m,n;
+    cin>>m>>n;
+
+    cout<<(m*n)/2<<endl;
+    
+    return 0;
+}
--- A_Elephant.cpp
+++ A_Elephant.cpp
@@ -5,6 +5,11 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
+//C++ Linux programming....
+//Research intern ..copporate apply to all internships and their companies contest 
+//Abhi thoda hard lg rha hai .. vaise bahut aasan hai ..
+
+
     int n;
     cin>>n;
     cout<<(n+4)/5<<endl;
