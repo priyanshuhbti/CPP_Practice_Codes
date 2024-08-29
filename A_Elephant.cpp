b3bad971f7552fc94a100633f1a9e683d458cec0
priyanshuhbti <priyanshusgvm@gmail.com>
2024-08-29 21:56:20 +0530

    main

---
+++ A_Elephant.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin>>n;
+    cout<<(n+4)/5<<endl;
+
+
+    return 0;
+}
