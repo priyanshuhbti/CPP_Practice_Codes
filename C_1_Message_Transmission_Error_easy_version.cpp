568886c311a4edc689291bf1ab49d34525098536
unknown <priyanshusgvm@gmail.com>
2024-09-11 15:48:26 +0530

    main

---
+++ C_1_Message_Transmission_Error_easy_version.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long n[10],i,m,S,q,l,h=(1LL<<41);
+string s,b("BSC");
+int main()
+{
+	for(cin >>s;i<s.size();i++)
+	{
+		long long z=b.find(s[i]);
+		n[z]++;
+	}
+	for(i=3;i<10;cin >>n[i++]);
+	while(l+1<h)
+	{
+		m=l+h>>1;
+		for(S=i=0;i<3;i++)
+		if((q=n[i]*m-n[3 + i])>0)
+		S+=q*n[6+i];
+		S>n[9]?h=m:l=m;
+	}
+	cout << l;
+}
