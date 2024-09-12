2c5beef258bd8a3e5706491aa70fb449123a8cef
Priyanshu Yadav <priyanshusgvm@gmail.com>
2024-09-12 13:24:24 +0530

    main

--- A_Theatre_Square.cpp
+++ A_Theatre_Square.cpp
@@ -5,13 +5,10 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n;
-    cin>>n;
-    if(n&1)cout<<0<<endl;
-    else{
-        cout<<(n-1)/4<<endl;
-        
-    }
+    int n,m,a;
+    cin>>n>>m>>a;
+    cout<<((n*m) %(a*a))<<endl;
+    
 
     return 0;
 }
