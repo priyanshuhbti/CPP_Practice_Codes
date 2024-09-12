9dca487b48d3b3a8e87e2d391f9c9bcb25e01399
Priyanshu Yadav <priyanshusgvm@gmail.com>
2024-09-12 13:39:12 +0530

    main

--- A_Theatre_Square.cpp
+++ A_Theatre_Square.cpp
@@ -7,8 +7,12 @@ int main() {
 
     int n,m,a;
     cin>>n>>m>>a;
-    cout<<((n*m) %(a*a))<<endl;
-    
+    // cout<<((n*m) %(a*a))<<endl;
+    long long res =0;
+    res +=(ceil(1.0 * n/a)* ceil(1.0*m/a));
+    cout<<res<<endl;
+    //Ceil is important and doing alg alg .is also very important ..
+        
 
     return 0;
 }
