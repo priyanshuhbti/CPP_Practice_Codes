1cd04ad011cc16137eb2e95783d54eb7c80817ec
Priyanshu Yadav <priyanshusgvm@gmail.com>
2024-09-17 23:31:18 +0530

    main

---
+++ A_K_divisible_Sum.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n,k;
+    cin>>n>>k;
+
+    long long cf =(n+k-1)/k;
+     k *= cf;
+     cout<<(k+n-1)/n <<endl;
+     
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
