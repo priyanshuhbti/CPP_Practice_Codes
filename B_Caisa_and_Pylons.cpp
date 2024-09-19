13b60f121df1dcb313e5b3a2e38b461970a112d3
Priyanshu Yadav <priyanshusgvm@gmail.com>
2024-09-19 16:36:39 +0530

    amp

---
+++ B_Caisa_and_Pylons.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+        int mx=0;
+
+    int n;
+    cin>>n;
+    while(n--){
+        int b;
+        cin>>b;
+        mx=max(mx,b);
+
+    }
+    cout<<mx<<endl;
+
+
+    return 0;
+}
