54db0b8e6bfdf1fb56d9448496b4a49f572bdbb4
Priyanshu Yadav <priyanshusgvm@gmail.com>
2024-11-20 22:41:39 +0530

    main

--- B_Medians.cpp
+++ B_Medians.cpp
@@ -1,89 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
-      
-/* OM NAMO NARAYANA */
-/* Code written by Priyanshu Yadav  */
 
-/* TYPES  */
 #define ll long long
-#define pii pair<int, int>
-#define pll pair<long long, long long>
 #define vi vector<int>
-#define vll vector<long long>
-#define mii map<int, int>
-#define si set<int>
-#define sc set<char>
 
-/* FUNCTIONS */
-#define f(i, s, e) for (long long int i = s; i < e; i++)
-#define cf(i, s, e) for (long long int i = s; i <= e; i++)
-#define rf(i, e, s) for (long long int i = e - 1; i >= s; i--)
-#define pb push_back
-#define eb emplace_back
+#define f(i, s, e) for (long long i = s; i < e; i++)
+#define yes() { cout << "YES\n"; }
+#define no() { cout << "NO\n"; }
 
-/* PRINTS */
-template <class T>
-void print_v(vector<T> &v) { cout << "{"; for (auto x : v) cout << x << ","; cout << "\b}"; }
+int main() {
+    ll t;
+    cin >> t;
+    while (t--) {
+        ll n;
+        cin >> n;
+        
+        vi a(n), b(n);
+        f(i, 0, n) cin >> a[i];
+        f(i, 0, n) cin >> b[i];
 
-/* UTILS */
-#define MOD 1000000007
-#define PI 3.1415926535897932384626433832795
-#define read(type) readInt<type>()
-ll min(ll a, int b) { if (a < b) return a; return b; }
-ll min(int a, ll b) { if (a < b) return a; return b; }
-ll max(ll a, int b) { if (a > b) return a; return b; }
-ll max(int a, ll b) { if (a > b) return a; return b; }
-ll gcd(ll a, ll b) { if (b == 0) return a; return gcd(b, a % b); }
-ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
-string to_upper(string a) { for (int i = 0; i < (int)a.size(); ++i) if (a[i] >= 'a' && a[i] <= 'z') a[i] -= 'a' - 'A'; return a; }
-string to_lower(string a) { for (int i = 0; i < (int)a.size(); ++i) if (a[i] >= 'A' && a[i] <= 'Z') a[i] += 'a' - 'A'; return a; }
-bool prime(ll a) { if (a == 1) return 0; for (int i = 2; i <= round(sqrt(a)); ++i) if (a % i == 0) return 0; return 1; }
-void yes() { cout << "YES\n"; }
-void no() { cout << "NO\n"; }
+        ll ma = -1, mb = -1, mi = -1;
+        ll sma = -1, smb = -1, smi = -1;
 
-/*  All Required define Pre-Processors and typedef Constants */
-typedef long int int32;
-typedef unsigned long int uint32;
-typedef long long int int64;
-typedef unsigned long long int uint64;
-
-/* clang-format on */
-
-/* Main()  function */
-int main()
-{
-
-    // #ifndef ONLINE_JUDGE
-    // freopen("input.txt", "r", stdin);
-    // #endif
-
-   ll t;
-cin >> t;
-while (t--)
-{
-   int n,k;
-        cin>>n>>k;
-        if(n==1){
-            cout<<1<<endl<<1<<endl;
-            continue;
-        }
-        if(k!=1 && k!=n){
-            if(k&1){
-                cout<<3<<endl;
-                cout<<1<<' '<<k-1<<' '<<k+2<<endl;
-            }
-            else{
-                cout<<3<<endl;
-                cout<<1<<' '<<k<<' '<<k+1<<endl;
+        f(i, 0, n) {
+            if (a[i] > ma) {
+                sma = ma;
+                smb = mb;
+                ma = a[i];
+                mb = b[i];
+                mi = i;
+            } else if (a[i] > sma) {
+                sma = a[i];
+                smb = b[i];
+                smi = i;
             }
         }
-        else{
-            cout<<-1<<endl;
-        }  
-    
-}
 
+        bool o1 = max(ma, mb) > max(sma, smb);
+        bool o2 = false;
+        f(i, 0, n) {
+            if (i == mi) continue;
+            if (b[i] > max(ma, mb)) {
+                o2 = true;
+                break;
+            }
+        }
+
+        if(o1 || o2) {yes();}
+        else cout<<"NO"<<endl;
+
+    }
     return 0;
 }
-
-
