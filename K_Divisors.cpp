5ff2cf3cebeeed8c719928c8deaab490d30ef0d6
Priyanshu Yadav <priyanshusgvm@gmail.com>
2024-12-31 22:29:03 +0530

    main

---
+++ K_Divisors.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> divisors;
+    for (int i = 1; i * i <= n; ++i) {
+        if (n % i == 0) {
+            divisors.push_back(i);
+            if (i * i != n) {
+                divisors.push_back(n / i);
+            }
+        }
+    }
+
+    sort(divisors.begin(), divisors.end());
+
+    for (int divisor : divisors) {
+        cout << divisor << endl;
+    }
+
+    return 0;
+}
