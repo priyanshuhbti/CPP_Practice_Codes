061c58d6e0b439905a9af2d7bc30bcc80b5d49ec
Priyanshu Yadav <priyanshusgvm@gmail.com>
2025-02-04 23:56:44 +0530

    main

---
+++ C_Sum_of_Cubes.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+      
+/* OM NAMO NARAYANA */
+/* Code written by Priyanshu Yadav  */
+
+/* TYPES  */
+#define ll long long
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define vi vector<int>
+#define vll vector<long long>
+#define mii map<int, int>
+#define si set<int>
+#define sc set<char>
+
+
+/* PRINTS */
+template <class T>
+void print_v(vector<T> &v) { cout << "{"; for (auto x : v) cout << x << ","; cout << "\b}"; }
+
+/* UTILS */
+#define MOD 1000000007
+#define PI 3.1415926535897932384626433832795
+#define read(type) readInt<type>()
+ll min(ll a, int b) { if (a < b) return a; return b; }
+ll min(int a, ll b) { if (a < b) return a; return b; }
+ll max(ll a, int b) { if (a > b) return a; return b; }
+ll max(int a, ll b) { if (a > b) return a; return b; }
+ll gcd(ll a, ll b) { if (b == 0) return a; return gcd(b, a % b); }
+ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+string to_upper(string a) { for (int i = 0; i < (int)a.size(); ++i) if (a[i] >= 'a' && a[i] <= 'z') a[i] -= 'a' - 'A'; return a; }
+string to_lower(string a) { for (int i = 0; i < (int)a.size(); ++i) if (a[i] >= 'A' && a[i] <= 'Z') a[i] += 'a' - 'A'; return a; }
+bool prime(ll a) { if (a == 1) return 0; for (int i = 2; i <= round(sqrt(a)); ++i) if (a % i == 0) return 0; return 1; }
+void yes() { cout << "YES\n"; }
+void no() { cout << "NO\n"; }
+
+/* clang-format on */
+
+
+
+/* Main()  function */
+signed main()
+{
+
+       ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    //ek ll pura game kr skta h...
+
+ 
+    set<ll>cubes;
+    for(ll i = 1; i <= 10000; i++) cubes.insert(i * i * i);
+ 
+    ll tt; cin >> tt;
+ 
+    while(tt--)
+    {
+        ll n, okay = 0; cin >> n;
+ 
+        for(auto &i : cubes) if(cubes.count(n - i)) okay = 1;
+ 
+        cout << (okay ? "YES\n" : "NO\n");
+    }
+    return 0;
+}
+
+
